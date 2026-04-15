376c9c331b1b4b3cc509b3c78e8813016a896edc
M. Thio Saputra <132740827+flessan@users.noreply.github.com>
2026-04-15 15:39:52 +0800

    same fix

--- website/_data/wallpapers.js
+++ website/_data/wallpapers.js
@@ -8,14 +8,14 @@ module.exports = async function() {
     const data = await response.json();
     
     return data
-  .filter(item => item.type === "dir")
-  .map(folder => ({
-    name: folder.name,
-    folderName: folder.name,
-    // Menggunakan githack agar file HTML dari repo bisa dirender sempurna di iframe
-htmlUrl: `https://raw.githack.com/flessan/HTMLWallarper/main/wallpapers/${folder.name}/index.html`,
-    url: folder.html_url
-  }));
+      .filter(item => item.type === "dir")
+      .map(folder => ({
+        name: folder.name,
+        folderName: folder.name,
+        // Menggunakan githack agar file HTML dari repo bisa dirender sempurna di iframe
+        htmlUrl: `https://raw.githack.com/flessan/HTMLWallarper/main/wallpapers/${folder.name}/index.html`,
+        url: folder.html_url
+      }));
   } catch (e) {
     return [];
   }
